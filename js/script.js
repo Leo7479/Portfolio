16c223c3a02a7625eaa51999a809c1aa290d0648
Leo7479 <shubhrajitsarkar123ss@gmail.com>
2024-10-16 02:28:52 +0530

    Added the splash screen

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// Variables Section
-
-// var dayCheck = document.getElementById("day-mode-checkbox");
-// var nightCheck = document.getElementById("night-mode-checkbox");
-// var showhidemode = document.getElementById("showhidemode");
-// var showhidemodelabel = document.getElementById("showhidemodelabel");
-// var modemenu = document.getElementsByClassName("mode")[0];
 var logoName = document.getElementById("logoHeading");
 var tile = document.getElementsByClassName("tile")[0];
 var r = document.querySelector(":root");
@@ -16,37 +9,22 @@ var article1 = document.getElementById("personal-info");
 var horizontalScrollCards = document.getElementsByClassName("cards horizontal")
 var typewrittertext = document.getElementById("typewrittertext");
 
-// Logic Section
-// showhidemode.addEventListener('click',()=>{
-//     if(showhidemode.checked){
-//         modemenu.classList.add("hide");
-//         showhidemodelabel.textContent = "<";
-//     }else{
-//         modemenu.classList.remove("hide");
-//         showhidemodelabel.textContent = ">";
-//     }
-// })
-// dayCheck.addEventListener('click',()=>{
-//     nightCheck.checked = false;
-//     dayCheck.checked = true;
-//     dayCheck.classList.add("active");
-//     nightCheck.classList.remove("active");
-//     r.style.setProperty("--highlight-color-1", "indigo");
-//     r.style.setProperty("--background-color", "white");
-//     r.style.setProperty("--overlay-color","rgb(161,150,255)");
-//     document.body.style.color = "black";
-// })
-// nightCheck.addEventListener('click',()=>{
-//     dayCheck.checked = false;
-//     nightCheck.checked = true;
-//     nightCheck.classList.add("active");
-//     dayCheck.classList.remove("active");
-//     r.style.setProperty("--highlight-color-1", "#bf68ff");
-//     r.style.setProperty("--background-color", "#4b0082");
-//     r.style.setProperty("--overlay-color","rgb(57,211,235)");
-//     document.body.style.color = "white";
-// })
 
+var loaderSpan = document.getElementsByClassName('loaderspan');
+let jumpCounter = 0;
+setInterval(()=>{
+    loaderSpan[jumpCounter].classList.remove('jump');
+    jumpCounter = (jumpCounter+1)%4;
+    loaderSpan[jumpCounter].classList.add('jump');
+},200);
+window.addEventListener('load', ()=>{
+    const splashDiv = document.getElementById('splash');
+    splashDiv.style.display="none";
+    animatable.forEach((a)=>{observer.observe(a)});
+    registerButton.addEventListener("click", ()=>{
+        window.open("https://forms.gle/a2F854fL54QmoMjh6", "_blank");
+    });
+})
 
 const ob = new IntersectionObserver((entry)=>{
     entry.forEach((e)=>{
